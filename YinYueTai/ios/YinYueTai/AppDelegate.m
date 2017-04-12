44015a500d52cdcc4077c0186927caf4b8b8698b
芦彬 <516475711@qq.com>
2017-04-12 10:45:26 +0800

    升级react 。。。

--- YinYueTai/ios/YinYueTai/AppDelegate.m
+++ YinYueTai/ios/YinYueTai/AppDelegate.m
@@ -8,8 +8,8 @@
  */
 
 #import "AppDelegate.h"
-#import "RCTBundleURLProvider.h"
-#import "RCTRootView.h"
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 #import "Orientation.h"
 
 #import <AVFoundation/AVFoundation.h>  // react-native-video 引入的必要的package
