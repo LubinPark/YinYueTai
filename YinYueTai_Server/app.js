9c44268626fc9a23929c3018fe8bc2017d1039fd
芦彬 <516475711@qq.com>
2017-05-10 17:57:02 +0800

    添加微信机器人。

--- YinYueTai_Server/app.js
+++ YinYueTai_Server/app.js
@@ -62,7 +62,7 @@ app.get('/', function(req, res) {
 
 // 可以将一类的路由单独保存在一个文件中
 // app.use('/todos', require('./routes/todos'));
-app.use('/todos', require('./routes/todos'));
+app.use('/wechat', require('./routes/wechatBot'));
 
 app.use(function(req, res, next) {
   // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
---
+++ YinYueTai_Server/routes/wechatBot.js
@@ -0,0 +1,162 @@
+var router = require('express').Router();
+// 引用 wechat 库，详细请查看 https://github.com/node-webot/wechat
+var wechat = require('wechat');
+var config = {
+  token: 'weixin',
+  appid: 'wx51ba8ca8236cce6c',
+  encodingAESKey: 'XIcwpIqvgdQzgg3C7l5VQXk93TPSTlqWMmLF7KYfgzd'
+};
+
+var WechatAPI = require('wechat-api');
+var api = new WechatAPI('wx51ba8ca8236cce6c',
+  'f018b64d5495265472cc2c6e03622920');
+
+router.use('/', wechat(config.token).text(function(message, req, res, next) {
+  // message为文本内容
+  // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+  // CreateTime: '1359125035',
+  // MsgType: 'text',
+  // Content: 'http',
+  // MsgId: '5837397576500011341' }
+  var keyArray = ['你好', '约吗', '骚骚'];
+  var content = message.Content;
+  var keyIndex = keyArray.indexOf(content);
+  switch (keyIndex) {
+    case 0:
+      {
+        res.reply({
+          type: "text",
+          content: '您好，大家好才是真的好！'
+        });
+
+      }
+      break;
+    case 1:
+      {
+        res.reply({
+          type: "text",
+          content: '不约，不约，叔叔我们不约！'
+        });
+
+      }
+      break;
+      case 2:
+        {
+          res.reply({
+            type: "text",
+            content: '大傻x'
+          });
+
+        }
+        break;
+    default:
+      res.reply({
+        type: "text",
+        content: '服务器挂掉了，你的要求暂时无法满足…… 哈哈，开玩笑，正在开发。。请稍后。。。'
+      });
+      break;
+  }
+}).image(function(message, req, res, next) {
+  // message为图片内容
+  // { ToUserName: 'gh_d3e07d51b513',
+  // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+  // CreateTime: '1359124971',
+  // MsgType: 'image',
+  // PicUrl: 'http://mmsns.qpic.cn/mmsns/bfc815ygvIWcaaZlEXJV7NzhmA3Y2fc4eBOxLjpPI60Q1Q6ibYicwg/0',
+  // MediaId: 'media_id',
+  // MsgId: '5837397301622104395' }}).voice(function(message, req, res, next) {
+  // TODO
+}).voice(function(message, req, res, next) {
+  // message为音频内容
+  // { ToUserName: 'gh_d3e07d51b513',
+  // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+  // CreateTime: '1359125022',
+  // MsgType: 'voice',
+  // MediaId: 'OMYnpghh8fRfzHL8obuboDN9rmLig4s0xdpoNT6a5BoFZWufbE6srbCKc_bxduzS',
+  // Format: 'amr',
+  // MsgId: '5837397520665436492' }
+}).video(function(message, req, res, next) {
+  // message为视频内容
+  // { ToUserName: 'gh_d3e07d51b513',
+  // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+  // CreateTime: '1359125022',
+  // MsgType: 'video',
+  // MediaId: 'OMYnpghh8fRfzHL8obuboDN9rmLig4s0xdpoNT6a5BoFZWufbE6srbCKc_bxduzS',
+  // ThumbMediaId: 'media_id',
+  // MsgId: '5837397520665436492' }
+  // TODO
+}).shortvideo(function(message, req, res, next) {
+  // message为短视频内容
+  // { ToUserName: 'gh_d3e07d51b513',
+  // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+  // CreateTime: '1359125022',
+  // MsgType: 'shortvideo',
+  // MediaId: 'OMYnpghh8fRfzHL8obuboDN9rmLig4s0xdpoNT6a5BoFZWufbE6srbCKc_bxduzS',
+  // ThumbMediaId: 'media_id',
+  // MsgId: '5837397520665436492' }
+  // TODO
+}).location(function(message, req, res, next) {
+  // message为链接内容
+  // { ToUserName: 'gh_d3e07d51b513',
+  // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+  // CreateTime: '1359125022',
+  // MsgType: 'link',
+  // Title: '公众平台官网链接',
+  // Description: '公众平台官网链接',
+  // Url: 'http://1024.com/',
+  // MsgId: '5837397520665436492' }
+  // TODO
+}).link(function(message, req, res, next) {
+  // message为链接内容
+  // { ToUserName: 'gh_d3e07d51b513',
+  // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+  // CreateTime: '1359125022',
+  // MsgType: 'link',
+  // Title: '公众平台官网链接',
+  // Description: '公众平台官网链接',
+  // Url: 'http://1024.com/',
+  // MsgId: '5837397520665436492' }
+  // TODO
+}).event(function(message, req, res, next) {
+  // message为事件内容
+  // { ToUserName: 'gh_d3e07d51b513',
+  // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+  // CreateTime: '1359125022',
+  // MsgType: 'event',
+  // Event: 'LOCATION',
+  // Latitude: '23.137466',
+  // Longitude: '113.352425',
+  // Precision: '119.385040',
+  // MsgId: '5837397520665436492' }
+  // TODO
+}).device_text(function(message, req, res, next) {
+  // message为设备文本消息内容
+  // { ToUserName: 'gh_d3e07d51b513',
+  // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+  // CreateTime: '1359125022',
+  // MsgType: 'device_text',
+  // DeviceType: 'gh_d3e07d51b513'
+  // DeviceID: 'dev1234abcd',
+  // Content: 'd2hvc3lvdXJkYWRkeQ==',
+  // SessionID: '9394',
+  // MsgId: '5837397520665436492',
+  // OpenID: 'oPKu7jgOibOA-De4u8J2RuNKpZRw' }
+  // TODO
+}).device_event(function(message, req, res, next) {
+  // message为设备事件内容
+  // { ToUserName: 'gh_d3e07d51b513',
+  // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+  // CreateTime: '1359125022',
+  // MsgType: 'device_event',
+  // Event: 'bind'
+  // DeviceType: 'gh_d3e07d51b513'
+  // DeviceID: 'dev1234abcd',
+  // OpType : 0, //Event为subscribe_status/unsubscribe_status时存在
+  // Content: 'd2hvc3lvdXJkYWRkeQ==', //Event不为subscribe_status/unsubscribe_status时存在
+  // SessionID: '9394',
+  // MsgId: '5837397520665436492',
+  // OpenID: 'oPKu7jgOibOA-De4u8J2RuNKpZRw' }
+  // TODO
+}).middlewarify());
+
+module.exports = router;
