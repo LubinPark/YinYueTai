5ffd49391f6db3e2b58dca5555349213defb5904
芦彬 <516475711@qq.com>
2017-05-17 18:21:16 +0800

    添加webpack

---
+++ yinyuetaireact/src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div style={{'textAlign': 'center'}}>
+        <h2>标题</h2>
+        <p>内容</p>
+      </div>
+    );
+  }
+};
--- yinyuetaireact/src/containers/app.js
+++ yinyuetaireact/src/containers/app.js
@@ -1,17 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Home from '../components/home'
 
 export default class App extends Component {
-
   render() {
     return (
-      <div style={{'textAlign': 'center'}}>
-          <div >
-            <h2>Welcome to React</h2>
-          </div>
-          <p >
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
-    );
+      <div><Home/></div>
+    )
   }
-};
+}
