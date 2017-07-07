c10a17b6cb752ed45d12ba1a788e7a0124fab7df
芦彬 <516475711@qq.com>
2017-07-07 18:02:13 +0800

    升级 3个 应用 yinyuetai  => 升级插件( react-native 未升级) webservice => 升级插件 修改 key web pack =>修改appid 等。正式在根目录companyAV.js(未上传)

--- YinYueTai_Server/config.js
+++ YinYueTai_Server/config.js
@@ -1,5 +1,13 @@
-const appId = 'wxf6586d282e5147bb'
-const appSecret = 'ae13fa4184393dd11d901c6a94db67e6'
+// 微信测试账号（自己的公众账号）
+// appid:wx51ba8ca8236cce6c
+// appSecret:19a12356edc31aed44c190d07f2306a4
+
+// 开放测试的账号（平台申请）
+// appid:wxf6586d282e5147bb
+// appSecret:ae13fa4184393dd11d901c6a94db67e6
+
+const appId = 'wx51ba8ca8236cce6c'
+const appSecret = '19a12356edc31aed44c190d07f2306a4'
 const encodingAESKey = 'XIcwpIqvgdQzgg3C7l5VQXk93TPSTlqWMmLF7KYfgzd'
 const token = 'weixin'
 
